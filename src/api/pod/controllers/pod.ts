f1189c3e1a5add861270d30e274dbdbcc5c83e2a
Darko Jankovic <darko.jankovic@poviolabs.com>
2022-08-18 09:17:00 +0200

    Add Pod resource

---
+++ src/api/pod/controllers/pod.ts
@@ -0,0 +1,7 @@
+/**
+ *  pod controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::pod.pod');
---
+++ src/api/pod/routes/pod.ts
@@ -0,0 +1,7 @@
+/**
+ * pod router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::pod.pod');
